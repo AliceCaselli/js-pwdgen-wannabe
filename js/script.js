8fdf83a2cd64981dcdec56c5ab05a6b35ce78e05
AliceCaselli <alicecaselli3@gmail.com>
2023-02-21 19:04:14 +0100

    fine

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// chiedere il nome
+// salvare il nome
+//chiedere il cognome
+//salvare il cognome
+//chiedere il colore
+//salvare il colore
+//inserire in testo tutti i dati + numero
+
+let userName = prompt("Ciao, come ti chiami?");
+
+let userSurname = prompt("Puoi dirmi anche il cognome?");
+
+let userColor = prompt("Ultima domanda: qual è il tuo colore preferito?");
+
+document.writeln( userName + userSurname + userColor + 3 + 4);
